0eec3d97128e7a5c4d0987df2087b031146bd003
Michael <MPWilkins95@gmail.com>
2021-02-06 11:54:43 -0700

    Changed value name

--- sudoku_puzzle.py
+++ sudoku_puzzle.py
@@ -13,27 +13,27 @@ board = [
 
 
 # sb = Sudoku Board
-def print_board(su):
+def print_board(sb):
     
-    for i in range(len(su)):
+    for i in range(len(sb)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
             
-        for k in range(len(su[0])):
+        for k in range(len(sb[0])):
             if k % 3 == 0 and k != 0:
                 print(" | ", end = "")
                 
             if k == 8:
-                print(su[i][k])
+                print(sb[i][k])
             else:
-                print(str(su[i][k]) + " ", end = "")
+                print(str(sb[i][k]) + " ", end = "")
             
             
-def find_empty(su):
+def find_empty(sb):
     
-    for i in range(len(su)):
-        for k in range(len(su[0])):
-            if su[i][k] == 0:
+    for i in range(len(sb)):
+        for k in range(len(sb[0])):
+            if sb[i][k] == 0:
                 return (i, k) # row, col
                 
 # print_board(board)
